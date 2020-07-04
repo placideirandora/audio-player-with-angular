9fc9eebc0b8a7d6aa71e78fae65ce27e3e1b422b
Placide IRANDORA <placideirandora@gmail.com>
2020-07-04 17:52:52 +0200

    Implement the Logic in the Player Component

---
+++ src/app/pages/player/player.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AudioService } from '../../services/audio.service';
+import { CloudService } from '../../services/cloud.service';
+import { StreamState } from '../../interfaces/stream-state';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.scss'],
+})
+export class PlayerComponent implements OnInit {
+  files: Array<any> = [];
+  state: StreamState;
+  currentFile: any = {};
+
+  constructor(
+    private audioService: AudioService,
+    private cloudService: CloudService
+  ) {}
+
+  ngOnInit(): void {
+    // get media files
+    this.cloudService.getFiles().subscribe((files) => {
+      this.files = files;
+    });
+
+    // listen to stream state
+    this.audioService.getState().subscribe((state) => {
+      this.state = state;
+    });
+  }
+
+  isFirstPlaying() {
+    return this.currentFile.index === 0;
+  }
+
+  isLastPlaying() {
+    return this.currentFile.index === this.files.length - 1;
+  }
+
+  playStream(url) {
+    this.audioService.playStream(url).subscribe((events) => {
+      // listening for fun here
+    });
+  }
+
+  openFile(file, index) {
+    this.currentFile = { index, file };
+    this.audioService.stop();
+    this.playStream(file.url);
+  }
+
+  pause() {
+    this.audioService.pause();
+  }
+
+  play() {
+    this.audioService.play();
+  }
+
+  stop() {
+    this.audioService.stop();
+  }
+
+  next() {
+    const index = this.currentFile.index + 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  previous() {
+    const index = this.currentFile.index - 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  onSliderChangeEnd(change) {
+    this.audioService.seekTo(change.value);
+  }
+}
